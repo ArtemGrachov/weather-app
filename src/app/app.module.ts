812cb0d59e60f653894997c8ed18d3efa362acab
Artem Grachov <grachov2195@gmail.com>
2019-08-14 09:36:09 +0300

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,15 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RootComponent } from './root/root.component';
+
+@NgModule({
+  declarations: [
+    RootComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [RootComponent]
+})
+export class AppModule { }
